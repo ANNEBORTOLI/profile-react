ef76795c4a58a5b6607fabeedfed7a4bcaec1b7a
ANNEBORTOLI <annebortoli@gmail.com>
2021-08-18 17:48:02 -0300

    conclusao component Repo

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import { Header } from "./components/Header";
 import { Avatar } from "./components/Avatar";
 import { Information } from "./components/Information";
 import { Partition } from "./components/Partition";
+import { Repo } from "./components/Repo";
+import { DivFlex } from "./components/DivFlex";
 
 function App() {
   const [dados, setDados] = useState({});
+  const [repos, setRepos] = useState([]);
 
   useEffect(() => {
     fetch("https://api.github.com/users/annebortoli")
@@ -14,15 +17,26 @@ function App() {
       .then((data) => setDados(data));
   }, []);
 
+  useEffect(() => {
+    fetch("https://api.github.com/users/annebortoli/repos")
+      .then((response) => response.json())
+      .then((data) => setRepos(data));
+  }, []);
+
   return (
     <>
       <Header name={dados.name} location={dados.location} />
-      <div className="main">
+      <DivFlex>
         <Information name="Followers" num={dados.followers} />
         <Avatar url={dados.avatar_url} />
         <Information name="Following" num={dados.following} />
-      </div>
+      </DivFlex>
       <Partition title="Repositories" />
+      <DivFlex>
+        {repos.map((repo, index) => (
+          <Repo key={index} name={repo.name} />
+        ))}
+      </DivFlex>
     </>
   );
 }
---
+++ src/components/DivFlex/index.jsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export function DivFlex(props) {
+  return <div className="div-flex">{props.children}</div>;
+}
